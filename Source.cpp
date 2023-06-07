ffa4954fd1cfc3ae9332335e1ca354825578e8a7
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2023-06-08 00:17:15 +0700

    fix wrong type default file extension for dll

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,15 @@
 #include "Injector.h"
 
 #define DEFAULT_EXTENSION L".exe"
+#define DEFAULT_DLL L".dll"
 const char* GetFileNameFromPath(char* input);
 
 int main() {
 	//----------------------------------------------------------------------------------------------
 	// SET OPENFILE DIALOGUE PROPERTIES
 	OPENFILENAME    ofn;
-	const wchar_t* FilterSpec = L"Application Files(*.exe)\0*.exe\0All Files(*.*)\0*.*\0";
-	const wchar_t* Title = L"Open....";
+	const wchar_t* FilterSpecExe = L"Application Files(*.exe)\0*.exe\0All Files(*.*)\0*.*\0";
+	const wchar_t* FilterSpecDll = L"Dll Files(*.dll)\0*.dll\0All Files(*.*)\0*.*\0";
 	wchar_t szFileName[MAX_PATH];
 	wchar_t szFileTitle[MAX_PATH];
 	int Result;
@@ -21,7 +22,6 @@ int main() {
 	/* fill in non-variant fields of OPENFILENAME struct. */
 	ofn.lStructSize = sizeof(OPENFILENAME);
 	ofn.hwndOwner = GetFocus();
-	ofn.lpstrFilter = FilterSpec;
 	ofn.lpstrCustomFilter = NULL;
 	ofn.nMaxCustFilter = 0;
 	ofn.nFilterIndex = 0;
@@ -30,24 +30,25 @@ int main() {
 	ofn.lpstrInitialDir = L"."; // Initial directory.
 	ofn.lpstrFileTitle = szFileTitle;
 	ofn.nMaxFileTitle = MAX_PATH;
-	ofn.lpstrTitle = Title;
 	ofn.lpstrDefExt = DEFAULT_EXTENSION;
 	ofn.Flags = OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
 	//----------------------------------------------------------------------------------------------
 
 	cout << "Selecting Application path" << endl;
-	Title = L"Selecting Application...";
+	ofn.lpstrTitle = L"Selecting Application...";
+	ofn.lpstrFilter = FilterSpecExe;
 	size_t numOfCharConverted = 0;
 	if (GetOpenFileName((LPOPENFILENAME)&ofn)) {
 		wcstombs_s(&numOfCharConverted,exeFilePath, ofn.lpstrFile, MAX_PATH);
 	}
 	
 	cout << "Selecting DLL path" << endl;
-	Title = L"Selecting DLL...";
+	ofn.lpstrTitle = L"Selecting DLL...";
+	ofn.lpstrFilter = FilterSpecDll;
+	ofn.lpstrDefExt = DEFAULT_DLL;
 	if (GetOpenFileName((LPOPENFILENAME)&ofn)) {
 		wcstombs_s(&numOfCharConverted,dllFilePath, ofn.lpstrFile, MAX_PATH);
 	}
-
 
 	Injector inject(GetFileNameFromPath(exeFilePath), dllFilePath);
 	inject.Inject();
